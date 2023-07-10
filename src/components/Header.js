4aeb1451aa2aa80a72778f0bf02bbb4efceb3e71
João Vareiro <joao.vareiro@ufms.br>
2023-07-10 12:40:39 -0400

    feat(Header) Adiciona sinalização da página atual.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import { useRef } from "react";
+import { useLocation, Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 import logo from "../assets/logo.svg";
 import "../styles/main.css";
 
 function Header() {
 	const navRef = useRef();
+
+	const location = useLocation();
+
+	const isActive = (pathname) => {
+	  return location.pathname === pathname ? 'active' : '';
+	};
 
 	const showHeader = () => {
 		navRef.current.classList.toggle(
@@ -14,33 +21,31 @@ function Header() {
 
 	return (
 		<header>
-			<img src={logo} alt="Logo" className="nav-logo"/>
-			<nav ref={navRef}>
-				<ul>
-					<li>
-						<a href="/">Home</a>
-					</li>
-					<li>
-						<a href="/analise">Análise de dados</a>
-					</li>
-					<li>
-						<a href="/quemsomos">Quem somos</a>
-					</li>
-					<li>
-						<a href="/documentacao">Documentação</a>
-					</li>
-				</ul>
-				<button className="nav-btn nav-close-btn" onClick={showHeader}>
-					<FaTimes />
-				</button>
-			</nav>
-			<button
-				className="nav-btn"
-				onClick={showHeader}>
-				<FaBars />
+		  <img src={logo} alt="Logo" className="nav-logo" />
+		  <nav ref={navRef}>
+			<ul>
+			  <li className={isActive('/')}>
+				<Link to="/">Home</Link>
+			  </li>
+			  <li className={isActive('/analise')}>
+				<Link to="/analise">Análise de dados</Link>
+			  </li>
+			  <li className={isActive('/quemsomos')}>
+				<Link to="/quemsomos">Quem somos</Link>
+			  </li>
+			  <li className={isActive('/documentacao')}>
+				<Link to="/documentacao">Documentação</Link>
+			  </li>
+			</ul>
+			<button className="nav-btn nav-close-btn" onClick={showHeader}>
+			  <FaTimes />
 			</button>
+		  </nav>
+		  <button className="nav-btn" onClick={showHeader}>
+			<FaBars />
+		  </button>
 		</header>
-	);
+	  );
 }
 
 export default Header;
