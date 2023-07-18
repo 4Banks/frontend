fc9a757c2034d23ed779b1ef108f920e2b73c536
João Vareiro <joao.vareiro@ufms.br>
2023-07-17 22:53:12 -0400

    feat(Análise de dados) Implementa envio do dataset.

--- src/pages/data-analysis/index.js
+++ src/pages/data-analysis/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import finance_home from '../../assets/finance_home.jpg';
+import "../../styles/global.css";
+import "../../styles/data-analysis.css";
+import CsvUpload from '../../components/CsvUpload';
 function DataAnalysis() {
   return (
     <div>
-      <h1>Análise de dados</h1>
+      <div className="data_analysis">
+        <div className="data_analysis_intro">
+          <div className="data_analysis_intro_info">
+          <h1 className="title_home">4banks</h1>
+          <p className='data_analysis_intro_info_text'>Descubra um mundo de possibilidades com o 4banks, nossa plataforma de análise de dados voltada para cientistas de dados do time de análise e detecção de fraudes em instituições financeiras. Com nossas ferramentas, você poderá explorar seus dados de transações bancárias de cartão de crédito e obter análises detalhadas, desde insights de alto nível até informações específicas. Nossa metodologia avançada facilita a tomada de decisões estratégicas, permitindo que você identifique padrões suspeitos e detecte atividades fraudulentas com precisão. Experimente agora e transforme seus dados em insights valiosos para proteger seus clientes e impulsionar a segurança financeira da sua instituição.</p>
+          <CsvUpload/>
+          </div>
+          <div className="data_analysis_intro_info_image_container">
+          <img src={finance_home} alt="finance_home" className='data_analysis_intro_info_image'/>
+          </div>
+          </div>
+      </div>
     </div>
   );
 }
