a55d6ead15f5ffe0a00fdfb86cff2954977bf6af
João Vareiro <joao.vareiro@ufms.br>
2023-07-19 15:06:08 -0400

    refactor(SelectionLabel.js) Refatora pop-up para melhor reusabilidade.

--- src/components/MultiSelection.js
+++ src/components/MultiSelection.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormGroup } from '@mui/material';
-import '../styles/multi-selection.css'
+import '../styles/multi-selection.css';
 import SelectionLabel from './SelectionLabel';
 import Checkbox from '@mui/material/Checkbox';
-
 
 function MultiSelection({ headerText, imageSrc, data }) {
   return (
     <div className='multi_selection_container'>
       <div className="multi_selection_header">
         <p className='multi_selection_header_text'>{headerText}</p>
-        <img src={imageSrc} alt="multi_selection"/>
+        <img src={imageSrc} alt="multi_selection" />
       </div>
       <div className="multi_selection_body">
         <FormGroup>
           {data.map((item) => (
-            <SelectionLabel key={item.item} item={item.item} description={item.description} selectionComponent={<Checkbox />} />
+            <SelectionLabel
+              key={item.item}
+              value={item.item}
+              description={item.description}
+              selectionComponent={<Checkbox />}
+            />
           ))}
         </FormGroup>
       </div>
