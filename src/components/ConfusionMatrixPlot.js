2c576d4ecd5b414463cbde1b4552f25b300ba36e
João Vareiro <joao.vareiro@ufms.br>
2023-07-27 11:28:15 -0400

    refactor(ConfusionMatrixPlot.js) Refatorando vistualização da matrix de confusão.

--- src/components/ConfusionMatrixPlot.js
+++ src/components/ConfusionMatrixPlot.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Plot from 'react-plotly.js';
 import '../styles/confusion-matrix-plot.css';
 
-const ConfusionMatrixPlot = ({ performanceMetrics, confusionMatrix, title, confusionMatrixDescription}) => {
 
-  const labels = Object.keys(performanceMetrics);
-  const confusionMatrixData = confusionMatrix.map((row) => Object.values(row));
+const ConfusionMatrixPlot = ({ confusionMatrix, title, confusionMatrixDescription}) => {
+
+  const labels = ['1', '0'];
+  const zValues = confusionMatrix;
 
   const matrixData = [
     {
       type: 'heatmap',
-      z: confusionMatrixData,
+      z: zValues,
       x: labels,
       y: labels,
       colorscale: 'Viridis',
@@ -18,23 +19,25 @@ const ConfusionMatrixPlot = ({ performanceMetrics, confusionMatrix, title, confu
   ];
 
   const matrixLayout = {
-    width: 600, height: 400,
+    width: 600,
+    height: 400,
     responsive: true,
     displayModeBar: false,
-    title: {title},
+    title: title || 'Confusion Matrix',
     xaxis: { title: 'Predicted Label' },
     yaxis: { title: 'True Label' },
   };
 
   return (
-      <div className="confusion_matrix">
-      <div className="confusion_matrix_info">
-      <p className='confusion_matrix_title'>{title}</p>
-      <p className='confusion_matrix_description'>{confusionMatrixDescription}</p>
-      </div>
-      <Plot data={matrixData} layout={matrixLayout} />
-      </div>
+    <div className="confusion_matrix">
+    <div className="confusion_matrix_info">
+    <p className='confusion_matrix_title'>{title}</p>
+    <p className='confusion_matrix_description'>{confusionMatrixDescription}</p>
+    </div>
+    <Plot data={matrixData} layout={matrixLayout} />
+    </div>
   );
 };
 
 export default ConfusionMatrixPlot;
+
