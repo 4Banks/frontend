7ccbf354f780db651ca5c0095baf06520ac92a6f
João Vareiro <joao.vareiro@ufms.br>
2023-07-28 20:42:04 -0400

    update(Quem somos) Atualiza descrição dos membros e aumenta tamanho da foto.

--- src/pages/about-us/index.js
+++ src/pages/about-us/index.js
@@ -18,7 +18,11 @@ function AboutUs() {
         </div>
         <AboutUsProfile
           name="Fabio Huang"
-          description="Aluno do segundo ano de engenharia de computação na UFMS. Bolsista no Laboratório de Inteligência Artificial da universidade, implementando processamento de imagens para classificação com modelos de atenção. Almeja seguir carreira na área de IA."
+          description={`Fábio Huang, de 19 anos, é estudante de Engenharia de Computação na Universidade Federal de Mato Grosso do Sul. Seu interesse está voltado para Machine Learning e suas aplicações, especialmente no processamento de imagens com tecnologias avançadas. Ele tem afinidade com o desenvolvimento e experimentação de modelos e técnicas de Deep Learning.
+
+                        No projeto 4banks, Fábio trabalhou na pesquisa e implementação de algoritmos de Data Science, sendo responsável pela escolha dos métodos aplicados. Além disso, ele participou de diversos programas acadêmicos, destacando-se sua participação em uma Iniciação Científica (IC) no Laboratório de Inteligência Artificial (LIA) em colaboração com o Ministério da Educação. Nesse projeto, o objetivo era desenvolver uma aplicação de educação cidadã para classificar espécies da fauna e flora da Mata Atlântica.
+
+                        Fábio demonstra habilidades em dinâmicas de equipe e comunicação, além de ser uma pessoa ambiciosa e apaixonada por aprender, abrangendo não somente as áreas de seu interesse específico.`}
           url_photo={fabio_huang}
           link_linkedin="https://www.linkedin.com/in/f%C3%A1bio-huang-0aa05b257/"
           link_github="https://github.com/FabioHuang"
@@ -27,12 +31,12 @@ function AboutUs() {
           name="João Pedro Vareiro"
           description={`João Vareiro tem 20 anos e é estudante de Engenharia de Software na Universidade Federal de Mato Grosso do Sul, com Média Geral Acadêmica de 8,93/10. Seu principal interesse na área de TI é a Inteligência Artificial.
           
-          Atualmente trabalha no Laboratório de Inteligência Artificial da UFMS, atuando na gestão de projetos e no desenvolvimento de modelos de visão computacional focados na detecção e classificação da fauna e flora da mata atlântica. Além disso, contribui no desenvolvimento do back-end e realiza integração contínua e entrega contínua nos sistemas de avaliação e inscrição da Feira de Tecnologias, Engenharias e Ciências de Mato Grosso do Sul.
+                        Atualmente trabalha no Laboratório de Inteligência Artificial da UFMS, atuando na gestão de projetos e no desenvolvimento de modelos de visão computacional focados na detecção e classificação da fauna e flora da mata atlântica. Além disso, contribui no desenvolvimento do back-end e realiza integração contínua e entrega contínua nos sistemas de avaliação e inscrição da Feira de Tecnologias, Engenharias e Ciências de Mato Grosso do Sul.
 
-No projeto 4banks, desenvolveu o front-end e atuou no desenvolvimento do protótipo e na descoberta do produto.
+                        No projeto 4banks, desenvolveu o front-end e atuou no desenvolvimento do protótipo e na descoberta do produto.
 
-Dentre suas experiências concluídas, trabalhou como trainee full-stack na SENAI Soluções Digitais, adquirindo conhecimentos em Java EE, Docker e Scrum. Também atuou como desenvolvedor front-end na Mega Júnior, onde desenvolveu aplicações em Vue.js. Além disso, foi membro da diretoria de Recursos Humanos, o que lhe proporcionou o desenvolvimento de habilidades interpessoais para resolver problemas entre os membros da empresa júnior, contribuindo para uma maior integração na equipe.
-`}
+                        Dentre suas experiências concluídas, trabalhou como trainee full-stack na SENAI Soluções Digitais, adquirindo conhecimentos em Java EE, Docker e Scrum. Também atuou como desenvolvedor front-end na Mega Júnior, onde desenvolveu aplicações em Vue.js. Além disso, foi membro da diretoria de Recursos Humanos, o que lhe proporcionou o desenvolvimento de habilidades interpessoais para resolver problemas entre os membros da empresa júnior, contribuindo para uma maior integração na equipe.
+                        `}
 
           url_photo={joao_vareiro}
           link_linkedin="https://www.linkedin.com/in/joaovareiro/"
@@ -47,6 +51,7 @@ Dentre suas experiências concluídas, trabalhou como trainee full-stack na SENA
                           No projeto 4banks, atuou na configuração do software, product owner, designer UI/UX e na documentação do projeto. 
                           
                           Já participou de diversos programas acadêmicos, como o PET Sistemas do Ministério da Educação, Elas programando, UNAPI (Universidade Aberta à Pessoa Idosa); e já atuou como representante discente do colegiado de Engenharia de Software; faz parte do LEDES (Laboratório de Desenvolvimento de Software) e do programa internacional de líderes Cargill Global Scholars.
+                          
                           É muito proativa, comunicativa, ambiciosa, determinada e quer realizar grandes feitos no mercado de computação, aliado a pesquisa e instituições de ensino.
                           `}
           url_photo={lourdes_oshiro}
@@ -57,9 +62,9 @@ Dentre suas experiências concluídas, trabalhou como trainee full-stack na SENA
           name="Tiago Santi"
           description={`Com 22 anos de idade, Tiago Clarintino Santi é um aluno de Engenharia de Software na Universidade Federal de Mato Grosso do Sul, entusiasta de Inteligência Artificial.
           
-          Nos últimos anos, dedicou-se à exploração de Visão Computacional e Processamento de Linguagem Natural, empregando essas habilidades na solução de problemas complexos por meio do desenvolvimento de software. Como parte integrante do Laboratório de Inteligência Artificial da UFMS, contribuiu para a pesquisa em Deep Learning durante seu período de iniciação científica. 
+                        Nos últimos anos, dedicou-se à exploração de Visão Computacional e Processamento de Linguagem Natural, empregando essas habilidades na solução de problemas complexos por meio do desenvolvimento de software. Como parte integrante do Laboratório de Inteligência Artificial da UFMS, contribuiu para a pesquisa em Deep Learning durante seu período de iniciação científica. 
           
-          No projeto 4banks, desempenhou os papéis de desenvolvedor backend, analista de dados e DevOps. Aliado à habilidade de comunicação, experiência em trabalho colaborativo e um forte desejo de crescimento, está determinado a aprimorar suas habilidades no campo do desenvolvimento de software, tanto na esfera acadêmica quanto no mercado.`}
+                        No projeto 4banks, desempenhou os papéis de desenvolvedor backend, analista de dados e DevOps. Aliado à habilidade de comunicação, experiência em trabalho colaborativo e um forte desejo de crescimento, está determinado a aprimorar suas habilidades no campo do desenvolvimento de software, tanto na esfera acadêmica quanto no mercado.`}
           url_photo={tiago_santi}
           link_linkedin="https://www.linkedin.com/in/tiago-santi/"
           link_github="https://github.com/TiagoSanti"
@@ -68,7 +73,7 @@ Dentre suas experiências concluídas, trabalhou como trainee full-stack na SENA
           name="Mentor - Mateus Mello"
           description={`Desenvolvedor de software há mais de 10 anos, formado pela UFSC em Sistemas de Informação. Atua na Neoway também como desenvolvedor de software no time de Plataforma de Dados responsável por toda ingestão dos dados na plataforma.
           
-          Acredita a tecnologia permite tornar as vidas das pessoas melhor e as empresas alcançarem novos patameres nos seus negócio.`}
+                        Acredita a tecnologia permite tornar as vidas das pessoas melhor e as empresas alcançarem novos patameres nos seus negócio.`}
           url_photo={mateus_mello}
           link_linkedin="https://www.linkedin.com/in/mateuspmello/"
           link_github="https://github.com/mateuspmello"
